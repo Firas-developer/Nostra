f30872e02ce613e400a4c06851e7c1cf3702d500
Muhammad Firas <148760953+Firas-developer@users.noreply.github.com>
2024-01-13 12:28:51 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,17 @@
+//Function for Search List
+var search = document.getElementById("search");
+var container = document.getElementById("collections");
+console.log(container)
+var ProductItems = container.querySelectorAll("div")
+search.addEventListener("keyup", function () {
+    var enteredValue = this.value.toUpperCase();
+    for (var i = 0; i < ProductItems.length; i++) {
+        var productname = ProductItems[i].querySelector("h1").textContent.toUpperCase();
+        if (productname.indexOf(enteredValue) < 0) {
+            ProductItems[i].style.display = "none";
+        } else {
+            ProductItems[i].style.display = "block";
+            console.log(ProductItems)
+        }
+    }
+});
